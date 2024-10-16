4d3c3b4f53c8dc1d25c75a4af5213482a0806f94
Swaroop <saiswaroop2674@gmail.com>
2024-10-16 17:19:58 +0530

    added basics of block chain and how to create blocks and chain them

---
+++ Readme.md
@@ -0,0 +1,8 @@
+A revision repository for anyone who'd like to refresh their BlockChain fundamentals.
+
+
+** Every line of code has been commented, to understand how to start the basics of BLOCKCHAIN.**\
+
+File to file progress will be updated , this repo is also referred by multiple youtube channels and websites I'll note them down below: 
+    --> {https://www.youtube.com/@simplyexplained}[Simply Explained]
+    --> {https://academy.binance.com/en/courses/track/beginner-track/blockchain-fundamentals/introduction-to-blockchain-technology}[Binance Academy]
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const sha256 = require('crypto-js/sha256');
+const sha256 = require('crypto-js/sha256'); //sha256 is a hashing function, think it of as a random string which is unique to match the chains of your previous and next block in a block chain
 class Block{
-    constructor(index, timestamp, data, previousHash = ''){
+    constructor(index, timestamp, data, previousHash = ''){ //constructors are nothing but building blocks you might use to build a house
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
@@ -8,7 +8,7 @@ class Block{
         this.hash = this.calculateHash();
     }
     calculateHash(){
-        return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();  //we convert the hash to string so that we can read it in a human readable format
     }
 }
 
@@ -17,31 +17,35 @@ class Blockchain{
         this.chain = [this.createGenesisBlock()];
     }
     createGenesisBlock(){
-        return new Block(0, "01/01/2024", "Genesis Block", "0");
+        return new Block(0, "01/01/2024", "Genesis Block", "0"); // sample data
     }
 
     getLatestBlock(){
-        return this.chain[this.chain.length - 1];   
+        return this.chain[this.chain.length - 1]; //to fetch the latest block in the chain or the last block in the chain   
     }
 
     addblock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+        newBlock.previousHash = this.getLatestBlock().hash; //this is the most important part of the block chain, this is where the chain is created
+        newBlock.hash = newBlock.calculateHash(); //this is the hash of the block which is calculated using the calculateHash function
+        this.chain.push(newBlock); //this is where the block is added to the chain
     }
 
-    isChainValid(){
+    isChainValid(){ // to validate the chains 
         for(let i=1;i<this.chain.length;i++){
-            const currentBlock = this.chain[i];
-            const previousblock = this.chain[i-1];
+            const currentBlock = this.chain[i]; 
+            const previousblock = this.chain[i-1];    
+            //the above two are the current block and the previous block in the chain 
             if(currentBlock.hash!== currentBlock.calculateHash())return false;
+            //if the current block hash is not equal to the calculated hash of the current block then return false
             if(currentBlock.previousHash!==previousblock.hash)return false;
+            //if the current block previous hash is not equal to the previous block hash then return false
         }
         return true;
     }
 }
 
 let myCoin = new Blockchain();
+//adding blocks
 myCoin.addblock(new Block(1, "01/02/2024", { amount: 4 }));
 myCoin.addblock(new Block(2, "01/03/2024", { amount: 10 }));
 
@@ -51,4 +55,4 @@ myCoin.chain[1].data = { amount: 100 };
 console.log('Is blockchain valid? ' + myCoin.isChainValid());
 myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
 
-// console.log(JSON.stringify(myCoin, null, 4));
+// console.log(JSON.stringify(myCoin, null, 4)); //use this to print the data in readable format
